c6e33fc6505b4c79a061db8a48789994595004f2
Domenick Lanuzza <93294662+Squirtle211@users.noreply.github.com>
2022-05-06 07:24:58 -0400

    Update 3 add domain user.ps1

--- 3 add domain user.ps1
+++ 3 add domain user.ps1
@@ -1,4 +1,5 @@
-﻿$Password = Read-Host -Prompt "Enter a password for this user" -AsSecureString
+#Prompts you to enter a password for the new user. Make it strong and don't forget it.
+$Password = Read-Host -Prompt "Enter a password for this user" -AsSecureString
 New-ADUser -Name "Group2" -Description "Domain admin account" -Enabled $true -AccountPassword $Password -PasswordNeverExpires $true
 Add-ADGroupMember -Identity "Domain Admins" -Members "Group2"
 
