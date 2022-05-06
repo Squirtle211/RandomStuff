314bfcf306584683b2e38d170f33e9d43c21ff94
Domenick Lanuzza <93294662+Squirtle211@users.noreply.github.com>
2022-05-06 07:16:22 -0400

    Update 2 install domain.ps1

--- 2 install domain.ps1
+++ 2 install domain.ps1
@@ -1,5 +1,3 @@
-﻿$Password = ConvertTo-SecureString -AsPlainText -Force -String = @Password123
-
 Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools 
 
 Install-ADDsForest -DomainName "group2.local" -InstallDNS 
