71131a6c97dda2eafa9e062d9ddcb34a6582436d
Domenick Lanuzza <93294662+Squirtle211@users.noreply.github.com>
2022-05-06 09:55:55 +0000

    Add files via upload

--- 1 change pc name_time_ip.ps1
+++ 1 change pc name_time_ip.ps1
@@ -1,15 +1,19 @@
 ﻿Rename-Computer -NewName "Group2DC"
 Set-TimeZone -Id "UTC"
 
+#Change to whatever IP settings the professor gives
 $ip = "192.168.237.10"
-$netmask = "255.255.255.0"
+$netmask = "24"
 $gateway = "192.168.237.2"
-$dns = "8.8.8.8"
+$dns = "127.0.0.1"
+$altdns = "8.8.8.8"
+$adapter = (Get-NetAdapter).InterfaceIndex
 
+Remove-NetIPAddress -InterfaceIndex $adapter
+Remove-NetRoute -InterfaceIndex $adapter -Confirm 
 
-$adapter = Get-WmiObject win32_NetworkAdapterConfiguration -Filter "IPEnabled = 'true'"
-$adapter.EnableStatic($ip,$netmask)
-$adapter.SetGateways($gateway)
-$adapter.SetDNSServerSearchOrder($dns)
+New-NetIPAddress -IPAddress $ip -DefaultGateway $gateway -PrefixLength $netmask -InterfaceIndex $adapter
+Set-DnsClientServerAddress -InterfaceIndex $adapter -ServerAddresses $dns, $altdns
+
 
 Restart-Computer 
