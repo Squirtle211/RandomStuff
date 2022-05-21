f195de91294a3d03f05a752f622ec443bbfc3f68
Domenick Lanuzza <93294662+Squirtle211@users.noreply.github.com>
2022-05-21 13:59:56 -0400

    Update 2 install domain.ps1

--- Scripts/2 install domain.ps1
+++ Scripts/2 install domain.ps1
@@ -1,7 +1,7 @@
 Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools 
 
 #This will prompt you to create a password for Directory Services Restore Mode. Make it and strong password and don't forget it.
-Install-ADDsForest -DomainName "group2.local" -InstallDNS 
+Install-ADDsForest -DomainName "group4.local" 
 
 Restart-Computer
 
