981933dc44f8dd1ca3c3603f4b40f3041ca96a72
Domenick Lanuzza <93294662+Squirtle211@users.noreply.github.com>
2022-05-21 14:04:04 -0400

    Update and rename 1 change pc name_time_ip.ps1 to change pc name_time_ip.ps1

--- Scripts/change pc name_time_ip.ps1
+++ Scripts/change pc name_time_ip.ps1
@@ -1,11 +1,11 @@
-﻿Rename-Computer -NewName "Group2DC"
+﻿Rename-Computer -NewName "Group4VM2"
 Set-TimeZone -Id "UTC"
 
 #Change to whatever IP settings the professor gives
 $ip = "192.168.237.10"
 $netmask = "24"
-$gateway = "192.168.237.2"
-$dns = "127.0.0.1"
+$gateway = "192.168.2.1"
+$dns = "192.168.2.115"
 $altdns = "8.8.8.8"
 $adapter = (Get-NetAdapter).InterfaceIndex
 
